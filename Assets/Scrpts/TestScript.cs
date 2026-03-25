d5ed572d2edc1847039471031bfdd021a8d35fe4
mushroom0601 <maplekaz@naver.com>
2026-03-25 14:10:39 +0900

    Test branch Tree

---
+++ Assets/Scrpts/TestScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaTestScript : MonoBehaviour
+{
+    //Start is called before the first frame ypdate
+    void Start()
+    {
+        Debug.Log("Developer Tree Test");
+    }
+
+    //Update is called once per frame
+    private void Update()
+    {
+
+    }
+
+}
+
